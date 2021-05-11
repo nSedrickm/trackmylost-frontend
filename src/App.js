789832f33ddbb07b476fadede87cd33546eaec9c
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-05-11 23:22:47 +0100

    update : add header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import "tailwindcss/dist/base.css";
 import './App.css';
+
+import { BrowserRouter as Router } from "react-router-dom";
+import ScrollToTop from "helpers/ScrollToTop";
+import AnimationRevealPage from "helpers/AnimationRevealPage";
+import Header from "components/headers/Header";
 
 function App() {
   return (
-    <p>App</p>
+    <Router>
+      <ScrollToTop>
+        <AnimationRevealPage>
+          <Header tw="max-w-none" />
+        </AnimationRevealPage>
+      </ScrollToTop>
+    </Router>
+
   );
 }
 
--- src/helpers/AnimationRevealPage.js
+++ src/helpers/AnimationRevealPage.js
@@ -4,7 +4,7 @@ import tw from "twin.macro";
 /* framer-motion and useInView here are used to animate the sections in when we reach them in the viewport
  */
 import { motion } from "framer-motion";
-import useInView from "use-in-view";
+import useInView from "@owaiswiz/use-in-view";
 
 const StyledDiv = tw.div`font-display min-h-screen text-secondary-500 p-4 md:p-8 overflow-hidden`;
 function AnimationReveal({ disabled, children }) {
@@ -50,8 +50,10 @@ function AnimatedSlideInComponent({ direction = "left", offset = 10, children })
   );
 }
 
-export default props => (
+const AnimationRevealPage = (props) => (
   <StyledDiv className="App">
     <AnimationReveal {...props} />
   </StyledDiv>
 );
+
+export default AnimationRevealPage;
