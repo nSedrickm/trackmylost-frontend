08ac82c8e210e9ba4352a58b699ce658039899d5
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-05-15 17:39:31 +0100

    refactor: add toast provider

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import "tailwindcss/dist/base.css";
 import './App.css';
 
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Toaster } from 'react-hot-toast';
 import ScrollToTop from "helpers/ScrollToTop";
 import AnimationRevealPage from "helpers/AnimationRevealPage";
 import Header from "components/headers/Header";
 import Footer from "components/footers/Footer";
-
 import HomePage from "pages/HomePage";
 import SearchPage from 'pages/SearchPage';
 import ReportPage from "pages/ReportPage";
@@ -20,6 +20,7 @@ function App() {
     <Router>
       <ScrollToTop>
         <AnimationRevealPage>
+          <Toaster />
           <Header tw="max-w-none" />
 
           <Switch>
