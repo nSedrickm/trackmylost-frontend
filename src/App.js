73205454561af9f5c049c431e924c6c2cbd3d5f4
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-05-22 16:05:15 +0100

    refactor(APP): add redirects for dashboard page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "rsuite/dist/styles/rsuite-default.css";
 import "tailwindcss/dist/base.css";
 import './App.css';
 
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Redirect, Route, Switch } from "react-router-dom";
 import { Toaster } from 'react-hot-toast';
 import ScrollToTop from "helpers/ScrollToTop";
 import AnimationRevealPage from "helpers/AnimationRevealPage";
@@ -16,8 +16,18 @@ import AlertPage from "pages/AlertPage";
 import NotFoundPage from "pages/NotFoundPage";
 import LoginPage from "pages/LoginPage";
 import RegisterPage from "pages/RegisterPage";
+import DashboardPage from "pages/DashboardPage";
+import { getToken } from "services/auth.service";
 
 function App() {
+
+  let token = getToken();
+  let isAuthorized = false;
+
+  if (token) {
+    isAuthorized = true;
+  }
+
   return (
     <Router>
       <ScrollToTop>
@@ -43,11 +53,15 @@ function App() {
             </Route>
 
             <Route exact path="/login">
-              <LoginPage />
+              {isAuthorized ? <Redirect to="/dashboard" /> : <LoginPage />}
             </Route>
 
             <Route exact path="/sign-up">
               <RegisterPage />
+            </Route>
+
+            <Route path="/dashboard">
+              {isAuthorized ? <DashboardPage /> : <Redirect to="/login" />}
             </Route>
 
             <Route>
