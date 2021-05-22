47acaf095a1a817c787179bd0b9982f0b05af985
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-05-22 23:21:44 +0100

    update: add fetch user and logout functions in auth service

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -2,9 +2,10 @@ import axios from 'axios';
 
 let API_URL = process.env.REACT_APP_API_URL;
 axios.defaults.baseURL = API_URL;
-
+axios.defaults.withCredentials = true;
+axios.get('/sanctum/csrf-cookie')
 export const registerUser = ({ first_name, last_name, phone_number, town, password }) => {
-    return axios.post("/register",
+    return axios.post("/api/register",
         {
             first_name: first_name,
             last_name: last_name,
@@ -15,12 +16,22 @@ export const registerUser = ({ first_name, last_name, phone_number, town, passwo
 }
 
 export const userLogin = async ({ phone_number, password }) => {
-    return axios.post("/login",
+    return axios.post("/api/login",
         {
             phone_number: phone_number,
             password: password
         }).then(response => response)
 };
+
+export const getUser = () => {
+    return axios.get("/api/user")
+        .then(response => response)
+}
+
+export const logOut = () => {
+    return axios.get("/api/logout")
+        .then(response => response)
+}
 
 export const getToken = () => {
     return sessionStorage.getItem('MYLOST_TOKEN');
