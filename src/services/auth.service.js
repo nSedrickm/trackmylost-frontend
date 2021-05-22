df36ae78894e165a53a3674dd8b79a438b112e8c
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-05-22 16:07:54 +0100

    chore: add auth service

---
+++ src/services/auth.service.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+
+let API_URL = process.env.REACT_APP_API_URL;
+axios.defaults.baseURL = API_URL;
+
+export const registerUser = ({ first_name, last_name, phone_number, town, password }) => {
+    return axios.post("/register",
+        {
+            first_name: first_name,
+            last_name: last_name,
+            phone_number: phone_number,
+            town: town,
+            password: password
+        }).then(response => response)
+}
+
+export const userLogin = async ({ phone_number, password }) => {
+    return axios.post("/login",
+        {
+            phone_number: phone_number,
+            password: password
+        }).then(response => response)
+};
+
+export const getToken = () => {
+    return sessionStorage.getItem('MYLOST_TOKEN');
+};
+
+export const setToken = (token) => {
+    sessionStorage.setItem('MYLOST_TOKEN', token);
+};
+
+export const removeToken = () => {
+    sessionStorage.removeItem('MYLOST_TOKEN');
+}
