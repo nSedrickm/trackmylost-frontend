d46450e8c32af2756e350c04dc5b34419f5bf27b
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-05-22 23:21:10 +0100

    update: add error handling and redirect after login

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -3,9 +3,9 @@ import tw from 'twin.macro';
 import AnimationRevealPage from "helpers/AnimationRevealPage";
 import { FiLogIn } from "react-icons/fi";
 
-// import { userLogin, setToken } from "services/api.service";
+import { userLogin, setToken } from "services/auth.service";
 import { Loader } from "rsuite";
-// import toast from 'react-hot-toast';
+import toast from 'react-hot-toast';
 
 const SubmitButton = tw.button`flex mx-auto items-center text-white bg-primary-500 border-0 py-3 px-12 focus:outline-none hover:bg-primary-700 rounded-4xl text-lg`;
 const Input = tw.input`w-full rounded border border-gray-300 focus:border-primary-500 focus:bg-white focus:ring-2 focus:ring-primary-200 text-base outline-none text-gray-700 py-2 px-4 leading-8 transition-colors duration-200 ease-in-out rounded-4xl placeholder-gray-400`;
@@ -22,37 +22,39 @@ const LoginPage = () => {
         evt.preventDefault();
 
         let formData = {
-            username: evt.target.elements.username?.value,
+            phone_number: evt.target.elements.phone_number?.value,
             password: evt.target.elements.password?.value,
         }
         console.log(formData);
 
         setLoading(true);
-        // agentLogin(formData)
-        //     .then(response => {
-        //         console.log(response);
-        //         toast.success(`Alert set successfully`);
-        //         setLoading(false);
-        //     })
-        //     .catch(error => {
-        //         if (error.response) {
-        //             // The request was made and the server responded with a status code
-        //             // that falls out of the range of 2xx
-        //             setLoading(false);
-        //             toast.error("An error occurred Please check your network and try again");
-        //         } else if (error.request) {
-        //             // The request was made but no response was received
-        //             // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-        //             // http.ClientRequest in node.js
-        //             setLoading(false);
-        //             toast.error("An error occurred Please check your network and try again");
-        //         } else {
-        //             // Something happened in setting up the request that triggered an Error
-        //             setLoading(false);
-        //             toast.error("An error occurred Please check your network and try again");
-
-        //         }
-        //     });
+        userLogin(formData)
+            .then(response => {
+                console.log(response.data);
+                setToken("Logged in");
+                toast.success(response.data.message);
+                setTimeout(() => {
+                    window.location.replace("/dashboard")
+                }, 1000)
+            })
+            .catch(error => {
+                if (error.response) {
+                    // The request was made and the server responded with a status code
+                    // that falls out of the range of 2xx
+                    setLoading(false);
+                    toast.error(error.response.data.message);
+                } else if (error.request) {
+                    // The request was made but no response was received
+                    // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+                    // http.ClientRequest in node.js
+                    setLoading(false);
+                    toast.error("An error occurred Please check your network and try again");
+                } else {
+                    // Something happened in setting up the request that triggered an Error
+                    setLoading(false);
+                    toast.error("An error occurred Please check your network and try again");
+                }
+            });
     }
 
     if (loading) {
@@ -73,13 +75,21 @@ const LoginPage = () => {
 
                         <Form onSubmit={(evt) => handleSubmit(evt)}>
                             <div tw="flex flex-wrap -m-2">
-                                <div tw="p-2 w-full mb-4">
+                                <div tw="p-2 w-full">
                                     <div tw="relative">
-                                        <Label htmlFor="username">Username</Label>
-                                        <Input required
-                                            type="text"
-                                            id="username"
-                                            name="username" placeholder="Enter username" />
+                                        <Label htmlFor="phone_number">Phone Number</Label>
+                                        <Input
+                                            required
+                                            type="tel"
+                                            id="phone_number"
+                                            name="phone_number"
+                                            placeholder="e.g 670020023"
+                                            size="9"
+                                            maxLength="9"
+                                            minLength="9"
+                                            pattern="(6|2)(2|3|[5-9])[0-9]{7}"
+                                        //with areacode pattern="(\+237|237)\s(6|2)(2|3|[5-9])[0-9]{7}"
+                                        />
                                     </div>
                                 </div>
                                 <div tw="p-2 w-full">
@@ -97,7 +107,7 @@ const LoginPage = () => {
                             <div tw="p-2 w-full mt-2">
                                 <SubmitButton type="submit">
                                     <FiLogIn /> &nbsp; login
-                                    </SubmitButton>
+                                </SubmitButton>
                             </div>
                         </Form>
                     </div>
