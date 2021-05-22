c0a3e2be0ae64a230e9a5214db3eccd327c63094
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-05-22 19:00:27 +0100

    refactor: update api service

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const API_URL = process.env.REACT_APP_API_URL;
-axios.defaults.baseURL = API_URL;
+axios.defaults.baseURL = API_URL + "/api";
+axios.defaults.withCredentials = true;
 
 export const registerItem = ({ document_type, first_name, other_names, phone_number, reward }) => {
     return axios.post("/items", {
