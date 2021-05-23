863306406d7744f3e411fb595087707856586cc6
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-05-23 13:09:01 +0100

    update: style nav header

--- src/components/headers/DashHeader.js
+++ src/components/headers/DashHeader.js
@@ -1,45 +1,38 @@
-import React, { useContext } from "react";
-import { motion } from "framer-motion";
+import React, { useState, useContext } from "react";
 import tw from "twin.macro";
-import styled from "styled-components";
-
 import useAnimatedNavToggler from "helpers/useAnimatedNavToggler.js";
 import logo from "images/logo.png";
-import {
-  FiBell,
-  FiLogOut,
-  FiMenu as MenuIcon,
-  FiX as CloseIcon
-} from "react-icons/fi";
+import { FiBell, FiLogOut, FiMenu as MenuIcon } from "react-icons/fi";
 import { Link } from 'react-router-dom';
 import { DashContext } from "Dashboard/DasbhoardContext";
+import { Drawer } from "rsuite";
 
 const MainHeader = tw.header`flex justify-between items-center max-w-none mx-auto bg-primary-500 `;
 const NavLinks = tw.div`inline-flex`;
 const NavButton = tw.button`h-20 inline-flex items-center transition duration-300 hocus:bg-primary-700 hocus:outline-none hocus:text-white text-white font-medium px-6 py-3 no-underline  appearance-none`;
 const NavLink = tw(Link)`
-    h-20 inline-flex items-center transition duration-300 hocus:bg-primary-700 hocus:outline-none hocus:text-white text-white font-medium px-6 py-3 no-underline hocus:no-underline  appearance-none
+    h-20 flex items-center transition duration-300 hocus:bg-primary-700 hocus:outline-none hocus:text-white text-white font-medium px-6 py-3 no-underline hocus:no-underline  appearance-none
+`;
+
+const MobileNavButton = tw.button`h-20 inline-flex w-1/2 items-center transition duration-300 hocus:bg-primary-700 hocus:outline-none hocus:text-white text-primary-500 font-medium px-6 py-3 no-underline appearance-none mb-4  shadow-lg border border-gray-100`;
+const MobileNavLink = tw(Link)`
+    h-20 flex items-center transition duration-300 hocus:bg-primary-700 hocus:outline-none hocus:text-white text-primary-500 font-medium px-6 py-3 no-underline hocus:no-underline  appearance-none mb-4 shadow-lg border border-gray-100
 `;
 const Logo = tw.img`h-20 py-1 px-4 bg-white`;
 const MobileNavLinksContainer = tw.nav`flex flex-1 items-center justify-between`;
 const NavToggle = tw(NavButton)`
   lg:hidden z-50 focus:outline-none hocus:text-white transition duration-300
-`;
-const MobileNavLinks = motion.custom(styled.div`
-  ${tw`lg:hidden z-50 fixed top-12 inset-x-4 p-8 border text-center rounded-lg text-gray-900 bg-white shadow-2xl`}
-  ${NavLinks} {
-    ${tw`flex flex-col items-center`}
-  }
-`);
+`
 
 const DesktopNavLinks = tw.nav`hidden lg:flex flex-1 justify-between items-center`;
 
 const DashHeader = () => {
 
   const { handleLogOut } = useContext(DashContext);
+  const [drawer, showDrawer] = useState(false);
 
   const collapseBreakpointClass = "lg"
-  const { showNavLinks, animation, toggleNavbar } = useAnimatedNavToggler();
+  const { toggleNavbar } = useAnimatedNavToggler();
   const collapseBreakpointCss = collapseBreakPointCssMap[collapseBreakpointClass];
 
   const defaultLinks = [
@@ -59,6 +52,19 @@ const DashHeader = () => {
     </React.Fragment>
   ];
 
+  const altLinks = [
+    <React.Fragment key="nav">
+      <MobileNavLink onClick={toggleNavbar} key="home" to="/dashboard/items">Items</MobileNavLink>
+      <MobileNavLink onClick={toggleNavbar} key="Alerts" to="/dashboard/alerts">Alerts</MobileNavLink>
+
+      <MobileNavButton>
+        <FiBell size={20} />
+      </MobileNavButton>
+      <MobileNavButton onClick={() => handleLogOut()}>
+        <FiLogOut size={20} /> &nbsp; logout
+        </MobileNavButton>
+    </React.Fragment>
+  ];
 
   return (
     <MainHeader>
@@ -69,12 +75,27 @@ const DashHeader = () => {
 
       <MobileNavLinksContainer css={collapseBreakpointCss.mobileNavLinksContainer}>
         <Logo src={logo} alt="logo" />
-        <MobileNavLinks initial={{ x: "150%", display: "none" }} animate={animation} css={collapseBreakpointCss.mobileNavLinks}>
-          {defaultLinks}
-        </MobileNavLinks>
-        <NavToggle onClick={toggleNavbar} className={showNavLinks ? "open" : "closed"}>
-          {showNavLinks ? <CloseIcon size={24} tw="mr-4 fixed right-4" /> : <MenuIcon size={24} />}
+        <NavToggle
+          onClick={() => {
+            toggleNavbar()
+            showDrawer(!drawer)
+          }} >
+          <MenuIcon size={24} />
         </NavToggle>
+        <Drawer
+          full
+          backdrop={true}
+          show={drawer}
+          onHide={() => showDrawer(!drawer)}
+        >
+          <Drawer.Header>
+            <Drawer.Title>Menu</Drawer.Title>
+          </Drawer.Header>
+          <Drawer.Body>
+            {altLinks}
+          </Drawer.Body>
+        </Drawer>
+
       </MobileNavLinksContainer>
     </MainHeader>
   );
