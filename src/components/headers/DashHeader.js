89f09964d9d876355f7230cb06b2c30dd6fc6fe3
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-05-23 10:32:54 +0100

    refactor: use dashcontext in Dash header for logout

--- src/components/headers/DashHeader.js
+++ src/components/headers/DashHeader.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import { motion } from "framer-motion";
 import tw from "twin.macro";
 import styled from "styled-components";
@@ -12,52 +12,33 @@ import {
   FiX as CloseIcon
 } from "react-icons/fi";
 import { Link } from 'react-router-dom';
+import { DashContext } from "Dashboard/DasbhoardContext";
 
 const MainHeader = tw.header`flex justify-between items-center max-w-none mx-auto bg-primary-500 `;
-
-export const NavLinks = tw.div`inline-flex`;
-
-/* hocus: stands for "on hover or focus"
- * hocus:bg-primary-700 will apply the bg-primary-700 class on hover or focus
- */
+const NavLinks = tw.div`inline-flex`;
 const NavButton = tw.button`h-20 inline-flex items-center transition duration-300 hocus:bg-primary-700 hocus:outline-none hocus:text-white text-white font-medium px-6 py-3 no-underline  appearance-none`;
-
-export const NavLink = tw(Link)`
+const NavLink = tw(Link)`
     h-20 inline-flex items-center transition duration-300 hocus:bg-primary-700 hocus:outline-none hocus:text-white text-white font-medium px-6 py-3 no-underline hocus:no-underline  appearance-none
 `;
-
-
-export const Logo = tw.img`h-20 py-1 px-4 bg-white`;
-
-export const MobileNavLinksContainer = tw.nav`flex flex-1 items-center justify-between`;
-export const NavToggle = tw(NavButton)`
+const Logo = tw.img`h-20 py-1 px-4 bg-white`;
+const MobileNavLinksContainer = tw.nav`flex flex-1 items-center justify-between`;
+const NavToggle = tw(NavButton)`
   lg:hidden z-50 focus:outline-none hocus:text-white transition duration-300
 `;
-export const MobileNavLinks = motion.custom(styled.div`
+const MobileNavLinks = motion.custom(styled.div`
   ${tw`lg:hidden z-50 fixed top-12 inset-x-4 p-8 border text-center rounded-lg text-gray-900 bg-white shadow-2xl`}
   ${NavLinks} {
     ${tw`flex flex-col items-center`}
   }
 `);
 
-export const DesktopNavLinks = tw.nav`hidden lg:flex flex-1 justify-between items-center`;
+const DesktopNavLinks = tw.nav`hidden lg:flex flex-1 justify-between items-center`;
 
-const DashHeader = ({ roundedHeaderButton = true, logoLink, links, className, collapseBreakpointClass = "lg" }) => {
-  /*
-   * This header component accepts an optionals "links" prop that specifies the links to render in the navbar.
-   * This links props should be an array of "NavLinks" components which is exported from this file.
-   * Each "NavLinks" component can contain any amount of "NavLink" component, also exported from this file.
-   * This allows this Header to be multi column.
-   * So If you pass only a single item in the array with only one NavLinks component as root, you will get 2 column header.
-   * Left part will be LogoLink, and the right part will be the the NavLinks component you
-   * supplied.
-   * Similarly if you pass 2 items in the links array, then you will get 3 columns, the left will be "LogoLink", the center will be the first "NavLinks" component in the array and the right will be the second "NavLinks" component in the links array.
-   * You can also choose to directly modify the links here by not passing any links from the parent component and
-   * changing the defaultLinks variable below below.
-   * If you manipulate links here, all the styling on the links is already done for you. If you pass links yourself though, you are responsible for styling the links or use the helper styled components that are defined here (NavLink)
-   */
+const DashHeader = () => {
 
+  const { handleLogOut } = useContext(DashContext);
 
+  const collapseBreakpointClass = "lg"
   const { showNavLinks, animation, toggleNavbar } = useAnimatedNavToggler();
   const collapseBreakpointCss = collapseBreakPointCssMap[collapseBreakpointClass];
 
@@ -68,26 +49,28 @@ const DashHeader = ({ roundedHeaderButton = true, logoLink, links, className, co
         <NavLink onClick={toggleNavbar} key="Alerts" to="/dashboard/alerts">Alerts</NavLink>
       </NavLinks>
       <NavLinks key={2}>
-        <NavButton><FiBell size={20} /></NavButton>
-        <NavButton><FiLogOut size={20} /> &nbsp; logout</NavButton>
+        <NavButton>
+          <FiBell size={20} />
+        </NavButton>
+        <NavButton onClick={() => handleLogOut()}>
+          <FiLogOut size={20} /> &nbsp; logout
+        </NavButton>
       </NavLinks>
     </React.Fragment>
   ];
 
 
-  links = links || defaultLinks;
-
   return (
-    <MainHeader className={className || "header-light"}>
+    <MainHeader>
       <DesktopNavLinks css={collapseBreakpointCss.desktopNavLinks}>
         <Logo src={logo} alt="logo" />
-        {links}
+        {defaultLinks}
       </DesktopNavLinks>
 
       <MobileNavLinksContainer css={collapseBreakpointCss.mobileNavLinksContainer}>
         <Logo src={logo} alt="logo" />
         <MobileNavLinks initial={{ x: "150%", display: "none" }} animate={animation} css={collapseBreakpointCss.mobileNavLinks}>
-          {links}
+          {defaultLinks}
         </MobileNavLinks>
         <NavToggle onClick={toggleNavbar} className={showNavLinks ? "open" : "closed"}>
           {showNavLinks ? <CloseIcon size={24} tw="mr-4 fixed right-4" /> : <MenuIcon size={24} />}
