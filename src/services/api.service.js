29a9cb8d535cd886d9035a60df576a946092b2f6
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-05-23 13:50:49 +0100

    refactor: add endpoints for items and alerts in api service

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
 
 const API_URL = process.env.REACT_APP_API_URL;
-axios.defaults.baseURL = API_URL + "/api";
+axios.defaults.baseURL = API_URL;
 axios.defaults.withCredentials = true;
+axios.get('/sanctum/csrf-cookie')
 
 export const registerItem = ({ document_type, first_name, other_names, phone_number, reward }) => {
-    return axios.post("/items", {
+    return axios.post("/api/items", {
         document_type: document_type,
         first_name: first_name,
         other_names: other_names,
@@ -15,18 +16,27 @@ export const registerItem = ({ document_type, first_name, other_names, phone_num
 }
 
 export const setAlert = ({ name, document_type, email, phone_number }) => {
-    return axios.post("/alerts", {
+    return axios.post("/api/alerts", {
         name: name,
         document_type: document_type,
         email: email,
         phone_number: phone_number
     }).then(response => response)
 }
+
 export const searchItem = ({ name }) => {
-    return axios.get("/items",
+    return axios.get("/api/items",
         {
             params: {
                 name: name
             }
         }).then(response => response)
+}
+
+export const getItems = () => {
+    return axios.get("/api/items").then(response => response)
+}
+
+export const getAlerts = () => {
+    return axios.get("/api/alerts").then(response => response)
 }
