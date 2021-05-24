bbebb61e92adde0293c2ea5052ad0ef1b809dbca
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-05-24 22:11:50 +0100

    update: move login to dashcontext

--- src/Dashboard/DasbhoardContext.js
+++ src/Dashboard/DasbhoardContext.js
@@ -1,7 +1,7 @@
 import AnimateLoader from "components/Loaders/AnimateLoader";
 import React, { useState, useContext } from "react";
 import toast from 'react-hot-toast';
-import { getUser, logOut, removeToken } from "services/auth.service";
+import { getUser, userLogin, logOut, removeToken, setToken } from "services/auth.service";
 
 const DashContext = React.createContext();
 
@@ -13,6 +13,45 @@ const DashProvider = ({ children }) => {
     const [isAuthorized, setAuthStatus] = useState(notAuthorized);
     const [userData, setUserData] = useState({});
     const [loading, setLoading] = useState(false);
+
+    const handleLogin = (evt) => {
+        evt.preventDefault();
+
+        let formData = {
+            phone_number: evt.target.elements.phone_number?.value,
+            password: evt.target.elements.password?.value,
+        }
+        console.log(formData);
+
+        setLoading(true);
+        userLogin(formData)
+            .then(response => {
+                console.log(response.data);
+                setToken("Logged in");
+                toast.success(response.data.message);
+                setTimeout(() => {
+                    window.location.replace("/agent/dashboard")
+                }, 1000)
+            })
+            .catch(error => {
+                if (error.response) {
+                    // The request was made and the server responded with a status code
+                    // that falls out of the range of 2xx
+                    setLoading(false);
+                    toast.error(error.response.data.message);
+                } else if (error.request) {
+                    // The request was made but no response was received
+                    // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+                    // http.ClientRequest in node.js
+                    setLoading(false);
+                    toast.error("An error occurred Please check your network and try again");
+                } else {
+                    // Something happened in setting up the request that triggered an Error
+                    setLoading(false);
+                    toast.error("An error occurred Please check your network and try again");
+                }
+            });
+    }
 
     const handleGetUser = () => {
         getUser().then(response => setUserData(response.data))
@@ -80,6 +119,7 @@ const DashProvider = ({ children }) => {
                 notAuthorized,
                 setAuthStatus,
                 userData,
+                handleLogin,
                 handleLogOut,
             }}
         >
