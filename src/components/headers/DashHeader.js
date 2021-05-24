e7598c74d6ca7ab38d6e3817bdeac24cba1ab780
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-05-24 15:35:40 +0100

    refactor: update dashboard Navbar

--- src/components/headers/DashHeader.js
+++ src/components/headers/DashHeader.js
@@ -7,77 +7,69 @@ import { Link } from 'react-router-dom';
 import { DashContext } from "Dashboard/DasbhoardContext";
 import { Drawer } from "rsuite";
 
-const MainHeader = tw.header`flex justify-between items-center max-w-none mx-auto bg-primary-500 `;
-const NavLinks = tw.div`inline-flex`;
+const MainHeader = tw.header`flex justify-between items-center max-w-none mx-auto bg-primary-500 shadow-lg`;
+const NavContainer = tw.div`inline-flex`;
 const NavButton = tw.button`h-20 inline-flex items-center transition duration-300 hocus:bg-primary-700 hocus:outline-none hocus:text-white text-white font-medium px-6 py-3 no-underline  appearance-none`;
 const NavLink = tw(Link)`
     h-20 flex items-center transition duration-300 hocus:bg-primary-700 hocus:outline-none hocus:text-white text-white font-medium px-6 py-3 no-underline hocus:no-underline  appearance-none
 `;
-
-const MobileNavButton = tw.button`h-20 inline-flex w-1/2 items-center transition duration-300 hocus:bg-primary-700 hocus:outline-none hocus:text-white text-primary-500 font-medium px-6 py-3 no-underline appearance-none mb-4  shadow-lg border border-gray-100`;
+const MobileNavButton = tw.button`h-20 inline-flex w-1/2 items-center transition duration-300 bg-primary-500 hocus:bg-primary-700 hocus:outline-none hocus:text-white text-white font-medium px-6 py-3 no-underline appearance-none mb-4  shadow-lg border border-gray-100`;
 const MobileNavLink = tw(Link)`
-    h-20 flex items-center transition duration-300 hocus:bg-primary-700 hocus:outline-none hocus:text-white text-primary-500 font-medium px-6 py-3 no-underline hocus:no-underline  appearance-none mb-4 shadow-lg border border-gray-100
+    h-20 flex items-center transition duration-300 bg-primary-500 hocus:bg-primary-700 hocus:outline-none hocus:text-white text-white font-medium px-6 py-3 no-underline hocus:no-underline  appearance-none mb-4 shadow-lg
 `;
 const Logo = tw.img`h-20 py-1 px-4 bg-white`;
-const MobileNavLinksContainer = tw.nav`flex flex-1 items-center justify-between`;
-const NavToggle = tw(NavButton)`
-  lg:hidden z-50 focus:outline-none hocus:text-white transition duration-300
-`
-
-const DesktopNavLinks = tw.nav`hidden lg:flex flex-1 justify-between items-center`;
+const DesktopNav = tw.nav`hidden lg:flex flex-1 justify-between items-center`;
+const MobileNav = tw.nav`lg:hidden flex flex-1 items-center justify-between`;
+const NavToggle = tw(NavButton)`lg:hidden z-50 focus:outline-none focus:bg-primary-500 hocus:text-white transition duration-300`;
 
 const DashHeader = () => {
 
   const { handleLogOut } = useContext(DashContext);
   const [drawer, showDrawer] = useState(false);
-
-  const collapseBreakpointClass = "lg"
   const { toggleNavbar } = useAnimatedNavToggler();
-  const collapseBreakpointCss = collapseBreakPointCssMap[collapseBreakpointClass];
 
   const defaultLinks = [
     <React.Fragment key="nav">
-      <NavLinks key={1}>
+      <NavContainer key={1}>
         <NavLink onClick={toggleNavbar} key="home" to="/dashboard/items">Items</NavLink>
         <NavLink onClick={toggleNavbar} key="Alerts" to="/dashboard/alerts">Alerts</NavLink>
-      </NavLinks>
-      <NavLinks key={2}>
+      </NavContainer>
+      <NavContainer key={2}>
         <NavButton>
           <FiBell size={20} />
         </NavButton>
         <NavButton onClick={() => handleLogOut()}>
           <FiLogOut size={20} /> &nbsp; logout
         </NavButton>
-      </NavLinks>
+      </NavContainer>
     </React.Fragment>
   ];
 
   const altLinks = [
     <React.Fragment key="nav">
-      <MobileNavLink onClick={toggleNavbar} key="home" to="/dashboard/items">Items</MobileNavLink>
-      <MobileNavLink onClick={toggleNavbar} key="Alerts" to="/dashboard/alerts">Alerts</MobileNavLink>
+      <MobileNavLink onClick={toggleNavbar} key="items" to="/dashboard/items">Items</MobileNavLink>
+      <MobileNavLink onClick={toggleNavbar} key="alerts" to="/dashboard/alerts">Alerts</MobileNavLink>
 
       <MobileNavButton>
         <FiBell size={20} />
       </MobileNavButton>
       <MobileNavButton onClick={() => handleLogOut()}>
         <FiLogOut size={20} /> &nbsp; logout
-        </MobileNavButton>
+      </MobileNavButton>
     </React.Fragment>
   ];
 
   return (
     <MainHeader>
-      <DesktopNavLinks css={collapseBreakpointCss.desktopNavLinks}>
+      <DesktopNav>
         <Logo src={logo} alt="logo" />
         {defaultLinks}
-      </DesktopNavLinks>
+      </DesktopNav>
 
-      <MobileNavLinksContainer css={collapseBreakpointCss.mobileNavLinksContainer}>
+      <MobileNav>
         <Logo src={logo} alt="logo" />
         <NavToggle
           onClick={() => {
-            toggleNavbar()
             showDrawer(!drawer)
           }} >
           <MenuIcon size={24} />
@@ -95,8 +87,7 @@ const DashHeader = () => {
             {altLinks}
           </Drawer.Body>
         </Drawer>
-
-      </MobileNavLinksContainer>
+      </MobileNav>
     </MainHeader>
   );
 };
@@ -107,27 +98,5 @@ const DashHeader = () => {
  * Its written like this because we are using macros and we can not insert dynamic variables in macros
  */
 
-const collapseBreakPointCssMap = {
-  sm: {
-    mobileNavLinks: tw`sm:hidden`,
-    desktopNavLinks: tw`sm:flex`,
-    mobileNavLinksContainer: tw`sm:hidden`
-  },
-  md: {
-    mobileNavLinks: tw`md:hidden`,
-    desktopNavLinks: tw`md:flex`,
-    mobileNavLinksContainer: tw`md:hidden`
-  },
-  lg: {
-    mobileNavLinks: tw`lg:hidden`,
-    desktopNavLinks: tw`lg:flex`,
-    mobileNavLinksContainer: tw`lg:hidden`
-  },
-  xl: {
-    mobileNavLinks: tw`lg:hidden`,
-    desktopNavLinks: tw`lg:flex`,
-    mobileNavLinksContainer: tw`lg:hidden`
-  }
-};
 
 export default DashHeader;
