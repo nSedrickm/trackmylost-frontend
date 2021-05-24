ef8bb6c77b8d2e5918e3525e5c6170e6026e2296
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-05-24 21:27:33 +0100

    update: dashboard may only contain dash routes

--- src/Dashboard/DashboardPage.js
+++ src/Dashboard/DashboardPage.js
@@ -2,31 +2,31 @@ import React from "react";
 import AnimationRevealPage from "helpers/AnimationRevealPage";
 import DashHeader from 'components/headers/DashHeader';
 import { Route, Switch } from "react-router-dom";
-import { DashProvider } from "Dashboard/DasbhoardContext";
 import ItemsPage from "Dashboard/ItemsPage";
 import AlertsPage from "Dashboard/AlertsPage";
 
 const DashboardPage = () => {
 
     return (
-        <DashProvider>
+        <>
             <DashHeader />
             <AnimationRevealPage>
                 <Switch>
-                    <Route exact path="/dashboard">
+                    <Route exact path="/agent/dashboard">
                         <ItemsPage />
                     </Route>
 
-                    <Route exact path="/dashboard/items">
+                    <Route exact path="/agent/dashboard/items">
                         <ItemsPage />
                     </Route>
 
-                    <Route exact path="/dashboard/alerts">
+                    <Route exact path="/agent/dashboard/alerts">
                         <AlertsPage />
                     </Route>
                 </Switch>
             </AnimationRevealPage>
-        </DashProvider>
+        </>
     )
 }
+
 export default DashboardPage;
