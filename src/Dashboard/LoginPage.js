1e658656178dd39b777d22775a2063aa6763bc6c
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-05-26 05:41:37 +0100

    feat: add password toggle for login form

--- src/Dashboard/LoginPage.js
+++ src/Dashboard/LoginPage.js
@@ -1,18 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import tw from 'twin.macro';
 import AnimationRevealPage from "helpers/AnimationRevealPage";
 import { useDashContext } from "Dashboard/DashboardContext";
-import { FiLogIn } from "react-icons/fi";
+import { FiEye, FiEyeOff, FiLogIn } from "react-icons/fi";
 
 const SubmitButton = tw.button`flex mx-auto items-center text-white bg-primary-500 border-0 py-3 px-12 focus:outline-none hover:bg-primary-700 rounded-4xl text-lg`;
 const Input = tw.input`w-full rounded border border-gray-300 focus:border-primary-500 focus:bg-white focus:ring-2 focus:ring-primary-200 text-base outline-none text-gray-700 py-2 px-4 leading-8 transition-colors duration-200 ease-in-out rounded-4xl placeholder-gray-400`;
 const Label = tw.label`leading-7 text-xs text-gray-600 uppercase tracking-wide font-medium block mb-2`;
 const Form = tw.form`mx-auto md:w-2/5 md:p-10 md:border border-primary-500 rounded-2xl`;
-
+const ToggleButton = tw.span`absolute inset-y-0 right-5 flex items-center  cursor-pointer`;
 
 const LoginPage = () => {
 
     const { handleLogin } = useDashContext();
+    const [toggle, setToggle] = useState(false);
 
     return (
         <AnimationRevealPage>
@@ -43,23 +44,27 @@ const LoginPage = () => {
                                 </div>
                             </div>
                             <div tw="p-2 w-full">
+                                <Label htmlFor="password">Password</Label>
                                 <div tw="relative">
-                                    <Label htmlFor="password">Password</Label>
                                     <Input required
-                                        type="password"
+                                        type={toggle ? "text" : "password"}
                                         id="password"
                                         name="password"
                                         placeholder="Enter password"
                                     />
+                                    <ToggleButton onClick={() => setToggle(!toggle)}>
+                                        {toggle ? <FiEyeOff size={20} /> : <FiEye size={20} />}
+                                    </ToggleButton>
                                 </div>
                             </div>
                         </div>
                         <div tw="p-2 w-full mt-2">
                             <SubmitButton type="submit">
                                 <FiLogIn /> &nbsp; login
-                                </SubmitButton>
+                            </SubmitButton>
                         </div>
                     </Form>
+
                 </div>
             </section>
         </AnimationRevealPage>
