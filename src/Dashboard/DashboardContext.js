e2db60fd7e7ca233d6bb04ccf06d2293aade8a21
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-05-29 20:37:13 +0100

    refactor: add item register function to dash context

--- src/Dashboard/DashboardContext.js
+++ src/Dashboard/DashboardContext.js
@@ -7,6 +7,7 @@ import AnimateLoader from "components/Loaders/AnimateLoader";
 
 import { Redirect, Route, Switch } from "react-router-dom";
 import { getUser, userLogin, logOut } from "services/auth.service";
+import { registerItem } from "services/api.service";
 import { getLocalState, setLocalState, clearLocalState, clearItems } from "services/storage.service";
 
 const DashContext = React.createContext();
@@ -173,6 +174,46 @@ const DashProvider = () => {
             });
     }
 
+    const handleRegisterItem = (evt) => {
+        evt.preventDefault();
+
+        let formData = {
+            document_type: evt.target.elements.document_type?.value,
+            first_name: evt.target.elements.first_name?.value,
+            other_names: evt.target.elements.other_names?.value,
+            phone_number: evt.target.elements.phone_number?.value,
+            reward: evt.target.elements.reward?.checked ? "yes" : "no"
+        }
+        console.log(formData);
+
+        setLoading(true);
+        registerItem(formData)
+            .then(response => {
+                console.log(response);
+                toast.success(`Item ${formData.document_type} registered`);
+                setLoading(false);
+            })
+            .catch(error => {
+                if (error.response) {
+                    // The request was made and the server responded with a status code
+                    // that falls out of the range of 2xx
+                    setLoading(false);
+                    toast.error("An error occurred Please check your network and try again");
+                } else if (error.request) {
+                    // The request was made but no response was received
+                    // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+                    // http.ClientRequest in node.js
+                    setLoading(false);
+                    toast.error("An error occurred Please check your network and try again");
+                } else {
+                    // Something happened in setting up the request that triggered an Error
+                    setLoading(false);
+                    toast.error("An error occurred Please check your network and try again");
+
+                }
+            });
+    }
+
     if (loading) {
         return (
             <AnimateLoader />
@@ -185,6 +226,7 @@ const DashProvider = () => {
                 state,
                 handleLogin,
                 handleLogOut,
+                handleRegisterItem,
             }}
         >
             <Switch>
