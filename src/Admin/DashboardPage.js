c60a56bc77821572d4fc5d11e6dbcec75e332ac5
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-05-30 02:11:42 +0100

    refactor: update imports for admin dashboard page

--- src/Admin/DashboardPage.js
+++ src/Admin/DashboardPage.js
@@ -1,14 +1,14 @@
 import React from "react";
 import AnimationRevealPage from "helpers/AnimationRevealPage";
 import DashHeader from 'components/headers/DashHeader';
-import ItemsPage from "Dashboard/ItemsPage";
-import AlertsPage from "Dashboard/AlertsPage";
+import ItemsPage from "Admin/ItemsPage";
+import AlertsPage from "Admin/AlertsPage";
 import { Route, Switch } from "react-router-dom";
-import { useDashContext } from "Dashboard/DashboardContext";
+import { useAdminContext } from "Admin/AdminContext";
 
 const DashboardPage = () => {
 
-    const { state } = useDashContext();
+    const { state } = useAdminContext();
     console.log(state);
 
     return (
@@ -16,15 +16,15 @@ const DashboardPage = () => {
             <DashHeader />
             <AnimationRevealPage>
                 <Switch>
-                    <Route exact path="/agent/dashboard">
+                    <Route exact path="/admin/dashboard">
                         <ItemsPage />
                     </Route>
 
-                    <Route exact path="/agent/dashboard/items">
+                    <Route exact path="/admin/dashboard/items">
                         <ItemsPage />
                     </Route>
 
-                    <Route exact path="/agent/dashboard/alerts">
+                    <Route exact path="/admin/dashboard/alerts">
                         <AlertsPage />
                     </Route>
                 </Switch>
