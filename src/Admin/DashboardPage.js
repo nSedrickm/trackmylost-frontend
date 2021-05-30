2d72f4c08961d0029bf26a8ab7b95ff5f38e56ba
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-05-30 01:33:06 +0100

    chore: move more admin specific files to admin

---
+++ src/Admin/DashboardPage.js
@@ -0,0 +1,36 @@
+import React from "react";
+import AnimationRevealPage from "helpers/AnimationRevealPage";
+import DashHeader from 'components/headers/DashHeader';
+import ItemsPage from "Dashboard/ItemsPage";
+import AlertsPage from "Dashboard/AlertsPage";
+import { Route, Switch } from "react-router-dom";
+import { useDashContext } from "Dashboard/DashboardContext";
+
+const DashboardPage = () => {
+
+    const { state } = useDashContext();
+    console.log(state);
+
+    return (
+        <>
+            <DashHeader />
+            <AnimationRevealPage>
+                <Switch>
+                    <Route exact path="/agent/dashboard">
+                        <ItemsPage />
+                    </Route>
+
+                    <Route exact path="/agent/dashboard/items">
+                        <ItemsPage />
+                    </Route>
+
+                    <Route exact path="/agent/dashboard/alerts">
+                        <AlertsPage />
+                    </Route>
+                </Switch>
+            </AnimationRevealPage>
+        </>
+    )
+}
+
+export default DashboardPage;
