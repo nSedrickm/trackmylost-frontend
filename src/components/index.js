d9f049142eb6112f038f894558d26f0712d934b1
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-06-06 16:07:04 +0100

    update: extract header bar component

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,7 @@
 import SearchBar from "components/SearchBar";
+import DashControlHeader from "components/headers/DashControlHeader";
 
 export {
-    SearchBar
+    SearchBar,
+    DashControlHeader
 }
