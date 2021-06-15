52189f61e79a6777942f570df1232f32bf7c20de
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-06-15 20:47:27 +0100

    refactor: alert page

--- src/pages/AlertPage.js
+++ src/pages/AlertPage.js
@@ -1,22 +1,14 @@
 import React, { useState } from 'react';
 import tw from 'twin.macro';
 import AnimationRevealPage from "helpers/AnimationRevealPage";
-import { FiBell, FiChevronDown } from "react-icons/fi";
-
-import { setAlert } from "services/api.service";
-import { Loader } from "rsuite";
+import AnimateLoader from 'components/Loaders/AnimateLoader';
 import toast from 'react-hot-toast';
+import { FiBell, FiChevronDown } from "react-icons/fi";
+import { setAlert } from "services/api.service";
+import { Input, Label, SubmitButton, Select, SelectToggle } from "components/General";
 
-const SubmitButton = tw.button`flex mx-auto items-center text-white bg-primary-500 border-0 py-3 px-12 focus:outline-none hover:bg-primary-700 rounded-4xl text-lg`;
-const Input = tw.input`w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-primary-500 focus:bg-white focus:ring-2 focus:ring-primary-200 text-base outline-none text-gray-700 py-2 px-4 leading-8 transition-colors duration-200 ease-in-out rounded-4xl placeholder-gray-400`;
-const Label = tw.label`leading-7 text-xs text-gray-600 uppercase tracking-wide font-medium block mb-2`;
 const Row = tw.div`flex flex-wrap -m-4 md:px-24`;
 const Form = tw.form`mx-auto md:w-1/2 md:my-24 md:border border-primary-500 md:p-8 rounded-2xl bg-white`;
-// const SubHeading = tw.h2`sm:text-3xl text-2xl font-bold mb-4 text-primary-500`;
-const Select = tw.select`block appearance-none w-full bg-gray-100  bg-opacity-50 border border-gray-300 text-gray-600 py-3 px-4 pr-8 rounded-4xl leading-tight focus:outline-none focus:bg-white focus:border-primary-500`;
-const SelectToggle = tw.div`pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700`;
-const LoadingContainer = tw.div`h-screen text-center`;
-
 
 const AlertPage = () => {
 
@@ -51,11 +43,7 @@ const AlertPage = () => {
     }
 
     if (loading) {
-        return (
-            <LoadingContainer>
-                <Loader backdrop size="md" content="processing please wait" vertical />
-            </LoadingContainer>
-        );
+        return <AnimateLoader />
     } else {
         return (
             <AnimationRevealPage>
